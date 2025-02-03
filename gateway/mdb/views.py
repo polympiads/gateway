3e2363238b0cdd3fef2c2cca7890956142d16b83
Eliott Tornassat <eliott.tornassat@epfl.ch>
2025-02-03 16:32:29 +0100

    search by secrets rather than by host for the time being

--- gateway/mdb/views.py
+++ gateway/mdb/views.py
@@ -1,12 +1,9 @@
 import datetime
 
-import django
-import django.core.exceptions
 from django.http import (
     HttpRequest,
     HttpResponse,
     HttpResponseBadRequest,
-    HttpResponseForbidden,
 )
 
 from .models import Machine
@@ -16,18 +13,13 @@ def mdbping(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         return HttpResponseBadRequest()
 
-    print(request.GET)
+    # print(request.GET)
     secret = request.GET.get("secret")
     if secret is None:
         return HttpResponseBadRequest()
 
-    try:
-        host = request.get_host()
-    except django.core.exceptions.DisallowedHost:
-        return HttpResponseForbidden()
-
-    machine = Machine.objects.filter(host=host).first()
-    if machine is None or machine.secret != secret:
+    machine = Machine.objects.filter(secret=secret).first()
+    if machine is None:
         return HttpResponseBadRequest()
 
     machine.last_ping = datetime.datetime.today()
